c5ad8e1984c4512b8ce72bc337edc692557bc714
EricCRodriguez <ecrod12@gmail.com>
2020-09-13 19:16:26 -0500

    almost completed backend, bug with pusher

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+  appId: "1072245",
+  key: "23fdb4b017247665ff5e",
+  secret: "20d7d947bc12368c3863",
+  cluster: "us2",
+  encrypted: true
+});
+
 //middleware
+app.use(express.json());
+app.use(cors());
 
 // DB config
 const connection_url =
@@ -17,8 +30,54 @@ mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A Change Occured", change);
+
+    if (change.opertationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
+
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 // listen
 app.listen(port, () => console.log("Listening on localhost:${port}"));
